080f8c5e861411768506a3f56f91db45ab2e1bcb
EveKS <vikkes@yandex.ru>
2017-09-04 22:10:06 +0700

    add components

---
+++ Components/Banner.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OPENINVESTMENS.Components
+{
+    public class Banner : ViewComponent
+    {
+        public IViewComponentResult Invoke(bool isMain)
+        {
+            return View();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.ResponseCompression;
+using System.IO.Compression;
 
 namespace OPENINVESTMENS
 {
@@ -19,13 +21,40 @@ namespace OPENINVESTMENS
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            #region Gzip Deflate
+            services.Configure<GzipCompressionProviderOptions>(options =>
+                options.Level = CompressionLevel.Optimal);
+
+            services.AddResponseCompression(options =>
+            {
+                options.EnableForHttps = true;
+
+                options.MimeTypes = new[]
+                {
+                    // General
+                    "text/plain",
+                    // Static files
+                    "text/css",
+                    "application/javascript",
+                    // MVC
+                    "text/html",
+                    "application/xml",
+                    "text/xml",
+                    "application/json",
+                    "text/json",
+                    // Custom
+                    "image/svg+xml"
+                };
+
+                options.Providers.Add<GzipCompressionProvider>();
+            });
+            #endregion
+
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
