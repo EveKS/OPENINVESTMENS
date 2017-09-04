77290086f56eadf041acd1a2e36a9bd6d921743d
EveKS <vikkes@yandex.ru>
2017-09-04 16:48:37 +0700

    initial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OPENINVESTMENS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Investors()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Rules()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Faq()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult News()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $(".open-text").click(function () {
+        let text = $('.more-text');
+        if (text.is('.more-text--close')) {
+            text.removeClass('more-text--close');
+            text.addClass('more-text--open');
+        } else {
+            text.removeClass('more-text--open');
+            text.addClass('more-text--close');
+        }
+    });
+
+    $(".open-list li").click(function () {
+        let text = $(this).find(".closed-text");
+        if (text.is(':visible')) {
+            text.slideUp();
+            $(this).addClass("act");
+        } else {
+            text.slideDown();
+            $(this).removeClass("act");
+        }
+    });
+});
